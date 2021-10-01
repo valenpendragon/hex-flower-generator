6dc4abdf8eb9d6f8f93643646cf9353a429ac118
Jeffrey Scott <valenpendragon@gmail.com>
2021-10-01 16:00:46 -0400

    Cleaned up some leftover code in Classes.py and some minor errors from typos in both files.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from lib/classes import Hex
+from lib.classes import Hex
 
 
 class HexFlower():
--- lib/classes.py
+++ lib/classes.py
@@ -1,4 +1,3 @@
-import yaml
 from collections import OrderedDict
 
 class Hex(OrderedDict):
@@ -31,24 +30,5 @@ class Hex(OrderedDict):
     def __str__(self):
         super().__str__(self)
 
-    def __repr__(se;f):
+    def __repr__(self):
         super().__repr__(self)
-
-class HexFlower():
-    """
-    This class takes a list of 19 Hex objects and adds them in the
-    order of the number value. They need to be numbered 1 through 19
-    in the Hex['number'] attribute
-    """
-    def __init__(self, *args):
-        self = []
-        for i in range(1, 20):
-            for hex in args:
-                if hex['number'] == i:
-                    self.extend(hex)
-
-    def __str__(self) -> str:
-        s = ''
-        for i in range(19):
-            s = s + 'Hex: {}'.format(self[i].__str__())
-        return s
