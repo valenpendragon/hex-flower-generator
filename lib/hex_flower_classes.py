9f0376dfb34495d939ecbfe24984b55a5de050a0
Jeffrey Scott <valenpendragon@gmail.com>
2022-01-08 07:10:18 -0500

    Built a skeleton for the hex flower classes in the order: Zone,  Hex, HexFlower. The file is named hex_flower_classes.py. First major step is to remove coordinates from the actual xml files. Screen locations in the GUI should be attributes of the canvas objects only. Verified that new xml sample files are still well-formed.

---
+++ lib/hex_flower_classes.py
@@ -0,0 +1,21 @@
+import os
+import math
+import random
+import csv
+
+class Hex(dict):
+    """
+    This class requires a dictionary with the following structure:
+        id: int, 
+    
+    """
+    pass
+
+#123456789b123456789c123456789d123456789e123456789f123456789g123456789h123456789
+
+
+class Zone():
+    pass
+
+class HexFlower():
+    pass
